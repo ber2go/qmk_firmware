3cf73e76a69cb66436830a3f8592f2cefbd6c587
ber2go <eubertgo@gmail.com>
2024-05-09 18:34:51 -0400

    Adjust hold duration

--- keyboards/crkbd/keymaps/ber2go/config.h
+++ keyboards/crkbd/keymaps/ber2go/config.h
@@ -29,7 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define QUICK_TAP_TERM 0
-//#define TAPPING_TERM 100
+#define TAPPING_TERM 250
+#define DYNAMIC_TAPPING_TERM_INCREMENT 50
 
 
 #ifdef RGBLIGHT_ENABLE
